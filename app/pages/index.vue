491ec696e273d188180f1868066a2373a75d3133
Cliford Pereira <cliford.pereira@gmail.com>
2026-07-19 21:14:23 +0530

    chore: update css path

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,6 @@
 <template>
     <div>
-        <h1>Database Resources</h1>
-        <ul>
-            <li><ULink active-class="font-bold text-primary-500" to="/resource/products">Products</ULink></li>
-            <li><ULink active-class="font-bold text-primary-500" to="/resource/customers">Customers</ULink></li>
-            <li><ULink active-class="font-bold text-primary-500" to="/resource/orders">Orders</ULink></li>
-            <li><ULink active-class="font-bold text-primary-500" to="/resource/orderitems">OrderItems</ULink></li>
-        </ul>
+        <h1>Nuxt Crud Table</h1>
+        <ULink to="/resource/products">Products</ULink>
     </div>
 </template>
